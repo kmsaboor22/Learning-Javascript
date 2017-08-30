f70c8d15b0e96873a304f47ea94ac9f54b1b928b
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-08-29 22:18:29 -0400

    p1

---
+++ Lesson-1.js
@@ -0,0 +1,30 @@
+/*
+Strings — Any grouping of keyboard characters (letters, spaces, numbers, or symbols) 
+surrounded by single quotes ('Hello') or double quotes ("World!"). 
+In the example above, 'New York City' is a string.
+*/
+console.log('JavaScript');
+/*
+Numbers — Any number, including numbers with decimals: 4, 1516
+*/
+console.log(33.7);
+/*
+Booleans — Either true or false, with no quotations.
+*/
+console.log(true);
+/*
+Null — Can only be null. It represents the absence of value.
+*/
+console.log(null);
+
+console.log(3 + 4); // Equals 7
+console.log(5 - 1); // Equals 4
+console.log(4 * 2); // Equals 8
+console.log(9 / 3); // Equals 3
+
+/*
+In the example above, the value saved to the length 
+property is retrieved from the string, 'Teaching the world how to code'. 
+The program prints 30 to the console, because Hello has five characters in it.
+*/
+console.log('Teaching the world how to code'.length)
