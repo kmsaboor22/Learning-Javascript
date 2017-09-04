7c8cb31eb9adc14612328a8625bdbcafb235890f
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-09-03 20:50:40 -0400

    1

--- Lesson-1.js
+++ Lesson-1.js
@@ -28,3 +28,9 @@ property is retrieved from the string, 'Teaching the world how to code'.
 The program prints 30 to the console, because Hello has five characters in it.
 */
 console.log('Teaching the world how to code'.length)
+
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
