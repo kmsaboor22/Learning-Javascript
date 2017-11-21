9b9599ccf9ff97254f5bf9eff55051c239091f26
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-11-21 17:50:54 -0500

    fiz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,20 @@
-for (i = 1; i <= 100; i++) {
-    if (i % 3) {
-        console.log("Fizz")
-    }
-    if (i % 5) {
-        console.log("Buzz")
-    } else(i % 3 & i % 5)
-    console.log("FizzBuzz")
+//print all numbers from 1 - 100
+// if  divisible by 3 Fizz
+// if by 5 Buzz
+// if by 3 and 5 FizzBuzz
+for (i = 1; i < 101; i++) {
 
+    var output = "";
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        output += "FizzBuzz";
+    } else if (i % 3 == 0) {
+        output += "Fizz";
+    } else if (i % 5 == 0) {
+        output += "Buzz";
+    } else {
+        output = i;
+    }
+    console.log(output)
 }
+
+// Refactor version
