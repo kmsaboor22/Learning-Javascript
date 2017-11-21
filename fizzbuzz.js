5f6e128b6ea47b9ecc9f205548177db123768875
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-11-21 17:00:53 -0500

    push

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+for (i = 1; i <= 100; i++) {
+    if (i % 3) {
+        console.log("Fizz")
+    }
+    if (i % 5) {
+        console.log("Buzz")
+    } else(i % 3 & i % 5)
+    console.log("FizzBuzz")
+
+}
---
+++ triangle.js
@@ -0,0 +1,10 @@
+/*
+    looping a * triangle 
+    1st setup the index
+    2nd checks the condition
+    3rd updates the index
+*/
+for (let i = "*"; i.length <= 10; i += "*") {
+    console.log(i)
+
+}
