3b56645692bf14a1e6946fd25d54f0a74d329f73
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-11-21 18:00:15 -0500

    learning... process

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,8 +2,8 @@
 // if  divisible by 3 Fizz
 // if by 5 Buzz
 // if by 3 and 5 FizzBuzz
-for (i = 1; i < 101; i++) {
 
+/*for (i = 1; i < 101; i++) {
     var output = "";
     if ((i % 3 == 0) && (i % 5 == 0)) {
         output += "FizzBuzz";
@@ -16,5 +16,15 @@ for (i = 1; i < 101; i++) {
     }
     console.log(output)
 }
+*/
 
 // Refactor version
+
+for (i = 1; i < 101; i++) {
+    var output = "";
+    if (i % 3 == 0)
+        output += "Fizz";
+    if (i % 5 == 0)
+        output += "Buzz";
+    console.log(output || i);
+}
