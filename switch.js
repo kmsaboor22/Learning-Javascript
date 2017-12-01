b20d3eec3a09813a81fb15f15432a74aca9d3a3a
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-12-01 13:10:42 -0500

    switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+let girl = 'kim';
+
+switch (girl) {
+    case 'natalie':
+        console.log('O');
+        break;
+    case 'ashley':
+        console.log('0');
+        break;
+    case 'Brooke':
+        console.log('I');
+    default:
+        console.log('Exit switch case');
+        break;
+}
