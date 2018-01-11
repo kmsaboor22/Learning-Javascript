e885787af6d87f5de1a531746ae2fcccda368f6f
Khalil Saboor <themaninthefoxhole@gmail.com>
2018-01-11 14:42:07 -0500

    Notes

---
+++ console.js
@@ -0,0 +1,31 @@
+// strings appears black
+console.log('Hello World');
+// numbers apears blues
+console.log(12324354435);
+
+let greeting = 'Hi';
+
+console.log(greeting);
+
+console.log([1, 4, 3, 2])
+console.log({ a: 1, b: 2 })
+console.table({ a: 1, b: 2, c: 3, d: 4 })
+
+console.error("ERROR")
+console.clear()
+console.warn("This is a warning")
+
+console.time("Hello")
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd("Hello")
