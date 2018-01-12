7d61b9a312d2da184e69f2357c4a5c0bcb63529e
Khalil Saboor <themaninthefoxhole@gmail.com>
2018-01-12 13:40:10 -0500

    Datatypes

---
+++ DataTypes.js
@@ -0,0 +1,35 @@
+//Date Types, 2 Types of Data Types
+
+// Primitive
+/*
+  Store on the stack. 
+  Stored directly in the location the var access.
+*/
+// Reference
+/*
+  Access by a reference
+  Objects that are stored on the heap
+  A pointer to a location in memory
+*/
+
+/*
+Reference Data Types
+  Objects
+  Arrays
+  Functions
+  Dates
+  Anything else...
+*/
+
+/*
+Six Primitive Data Types
+
+Strings
+Numbers
+Boolean
+Null
+Undefined
+(ES6) Symbols
+*/
+
+console.log(typeof 5);
