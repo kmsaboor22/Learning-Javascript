bca6f711ed16a21597e9b34225f122c06432789f
Khalil Saboor <themaninthefoxhole@gmail.com>
2018-01-16 21:07:54 -0500

    move to new file

--- jsSandBox/app.js
+++ jsSandBox/app.js
@@ -1,35 +1,5 @@
-//Date Types, 2 Types of Data Types
-
-// Primitive
 /*
-  Store on the stack. 
-  Stored directly in the location the var access.
+1. Take notes
+2. Cut and paste to new JS file.
+3. Repeat
 */
-// Reference
-/*
-  Access by a reference
-  Objects that are stored on the heap
-  A pointer to a location in memory
-*/
-
-/*
-Reference Data Types
-  Objects
-  Arrays
-  Functions
-  Dates
-  Anything else...
-*/
-
-/*
-Six Primitive Data Types
-
-Strings
-Numbers
-Boolean
-Null
-Undefined
-(ES6) Symbols
-*/
-
-console.log(typeof 5);
