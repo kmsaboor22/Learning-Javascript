88ba4cf2bb04d7f76c8df12afd8b2d0cb44bba0b
Khalil Saboor <themaninthefoxhole@gmail.com>
2018-01-18 17:11:37 -0500

    type convert

--- jsSandBox/app.js
+++ jsSandBox/app.js
@@ -3,3 +3,20 @@
 2. Cut and paste to new JS file.
 3. Repeat
 */
+
+let val;
+
+/*numbers to string
+
+val = 5;
+
+*/
+
+//turn into string by using the string func
+
+val = String(5);
+
+//Output
+console.log(val);
+console.log(typeof val);
+console.log(val.length)
